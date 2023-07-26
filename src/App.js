7fd25b4db07bcc9714b3ffd409a05dbeb24b376c
Shup-46 <102753110+Shup-46@users.noreply.github.com>
2023-07-26 06:10:52 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import "./app.scss";
+const Layout = () =>{
+  return(
+    <div className="app">
+       <Navbar/>
+       <Outlet/> 
+       <Footer/>
+    </div>
+  )
+}
+
+const router = createBrowserRouter(
+  [{
+     path: "/",
+     element:<Layout/>,
+     children:[
+      {
+        path:"/",
+        element:<Home/>
+
+      },
+      {
+        path: "/Products/:id",
+        element:<Products/>
+      },
+      {
+        path: "/Product/:id",
+        element:<Product/>
+      },
+     ]
+  },
+  
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <RouterProvider router={router} />  
     </div>
   );
 }
---
+++ src/pages/Product/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Product.scss"
+const Product = () => {
+  return (
+    <div className='Product'>
+      product
+    </div>
+  )
+}
+
+export default Product
